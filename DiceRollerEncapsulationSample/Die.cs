8d5e20cf4220de964c5999993ba32bbbe57d9990
Scyllizzy <babytink2002@hotmail.com>
2020-01-13 14:41:50 -0800

    Add project files.

---
+++ DiceRollerEncapsulationSample/Die.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRollerEncapsulationSample
+{
+    /// <summary>
+    /// Represents an individual 6-sided die
+    /// </summary>
+    class Die
+    {
+        // Static members get shared across all instances of this class. 
+        //Meaning all die now have access to the same random object
+        private static Random rand;
+
+        /// <summary>
+        /// The current value of the die (1-6)
+        /// </summary>
+        public byte FaceValue { get; private set; }
+
+        /// <summary>
+        /// Die that are held are not rolled
+        /// </summary>
+        public bool IsHeld { get; set; }
+
+        /// <summary>
+        /// Runs once before any die objects are created 
+        /// and initializes Random object once
+        /// </summary>
+        static Die()
+        {
+            rand = new Random();
+        }
+
+        public Die()
+        {
+            Roll();
+        }
+
+        /// <summary>
+        /// Generates a new random face value (1-6).
+        /// Sets the face value to the randomly generated value.
+        /// Returns the face value.
+        /// </summary>
+        /// <returns>Return face value (1-6)</returns>
+        public byte Roll()
+        {
+            //If current die is held, return the current value. It wont roll
+            if (IsHeld)
+            {
+                return FaceValue;
+            }
+
+            const byte MinValue = 1;
+            const byte MaxValue = 6;
+            //Offset is for the exclusive range of Random.Next()
+            const byte Offset = 1;
+
+            FaceValue = (byte)rand.Next(MinValue, MaxValue + Offset);
+            return FaceValue;
+        }
+    }
+}
---
+++ DiceRollerEncapsulationSample/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRollerEncapsulationSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Die[] dice = new Die[5];
+
+            InitDice(dice);
+            PrintFaceValues(dice);
+
+            //Hold first 3 dice
+            dice[0].IsHeld = true;
+            dice[1].IsHeld = true;
+            dice[2].IsHeld = true;
+
+            Console.WriteLine();
+
+            RollDice(dice);
+            PrintFaceValues(dice);
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Rolls all the die in given array.
+        /// </summary>
+        /// <param name="dice">Array of die</param>
+        private static void RollDice(Die[] dice)
+        {
+            for (int i = 0; i < dice.Length; i++)
+            {
+                dice[i].Roll();
+            }
+        }
+
+        /// <summary>
+        /// Prints the face values of the die in given array
+        /// </summary>
+        /// <param name="dice">Array of die</param>
+        private static void PrintFaceValues(Die[] dice)
+        {
+            for (int i = 0; i < dice.Length; i++)
+            {
+                Console.WriteLine(dice[i].FaceValue);
+            }
+        }
+
+        /// <summary>
+        /// Initializes all die in given array.
+        /// Assumes array has been given a length.
+        /// </summary>
+        /// <param name="dice">Array of die</param>
+        private static void InitDice(Die[] dice)
+        {
+            for (int i = 0; i < dice.Length; i++)
+            {
+                dice[i] = new Die();
+            }
+        }
+    }
+}
